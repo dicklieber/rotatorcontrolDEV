feed8e9479662ae6275416efa24b9822283dc0a7
Dick Lieber <dick@u505.com>
2022-11-12 13:18:29 -0600

    copyright

--- src/main/scala/RotatorManager.scala
+++ src/main/scala/RotatorManager.scala
@@ -6,6 +6,24 @@ import scalafx.application.{JFXApp3, Platform}
 import scalafx.scene.Scene
 import scalafx.scene.control.{Label, Menu, MenuBar, MenuItem}
 import scalafx.scene.layout.BorderPane
+/*
+ *   Copyright (C) 2022  Dick Lieber, WA9NNN
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 import scalafx.scene.paint.Color
 
 /** import _root_.scalafx.event.ActionEvent
