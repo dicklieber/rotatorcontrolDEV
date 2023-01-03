0a3f8e39b29347b62e99e8576d59dbe6f3840261
Dick Lieber <dick@u505.com>
2023-01-03 11:09:07 -0600

    TaskBar icon

--- src/main/scala/com/wa9nnn/rotator/RotatorControl.scala
+++ src/main/scala/com/wa9nnn/rotator/RotatorControl.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2023  Dick Lieber, WA9NNN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.wa9nnn.rotator
 
 import com.google.inject.{Guice, Injector}
@@ -5,14 +22,11 @@ import com.wa9nnn.rotator.arco.ArcoManager
 import com.wa9nnn.rotator.metrics.MetricsReporter
 import com.wa9nnn.rotator.ui.AboutDialog
 import com.wa9nnn.rotator.ui.config.ConfigEditorDialog
-import com.wa9nnn.util.TimeConverters
+import net.codingwell.scalaguice.InjectorExtensions.ScalaInjector
 import scalafx.application.{JFXApp3, Platform}
 import scalafx.scene.Scene
 import scalafx.scene.control.{Label, Menu, MenuBar, MenuItem}
 import scalafx.scene.layout.BorderPane
-import net.codingwell.scalaguice.InjectorExtensions.ScalaInjector
-
-import java.time.Instant
 
 /**
  * Main
@@ -50,16 +64,24 @@ object RotatorControl extends JFXApp3 {
     }
     injector = Guice.createInjector(new GuiceModule())
     val arcoManager: ArcoManager = injector.instance[ArcoManager]
+    val imagePath: String = s"/images/docIcon.png"
+
+    import java.awt.Taskbar
+    val taskbar: Taskbar = Taskbar.getTaskbar
+    import java.awt.{Image, Toolkit}
+
+    val image: Image = Toolkit.getDefaultToolkit.getImage(getClass.getResource(imagePath))
+    taskbar.setIconImage(image)
 
 
-    val secne: Scene = new Scene {
+    val scene: Scene = new Scene {
       val cssUrl: String = getClass.getResource("/rotatormanager.css").toExternalForm
       stylesheets.add(cssUrl)
 
       private val menuBar = new MenuBar {
         useSystemMenuBar = true
       }
-      val confgMenu = new Menu("Config") {
+      val configMenu = new Menu("Config") {
         items += editRotatorMenuItem
         items += metricsMenu
       }
@@ -67,19 +89,18 @@ object RotatorControl extends JFXApp3 {
         items += aboutDialogItem
       }
 
-      menuBar.menus = List(confgMenu, aboutMenu)
+      menuBar.menus = List(configMenu, aboutMenu)
       root = new BorderPane {
 
         top = menuBar
         center = arcoManager
         //          center = tabPane
 
-        val builtAt = TimeConverters.instantDisplayUTCLocal(Instant.ofEpochMilli(BuildInfo.builtAtMillis))
         bottom = new Label {
           text = s"Version ${BuildInfo.version}"
         }
       }
     }
-    stage.scene = secne
+    stage.scene = scene
   }
 }
